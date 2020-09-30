70a606242a1e7e48c3f7379cbfc4fc0db23a7f17
toufik6815 <64719882+toufik6815@users.noreply.github.com>
2020-09-30 12:45:33 +0600

    first commit

---
+++ src/components/Login/firebace.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB0SauD6Qr4Oy_nS6uDoVOyL8VzLrdsdE4",
+  authDomain: "burj-al-arabic.firebaseapp.com",
+  databaseURL: "https://burj-al-arabic.firebaseio.com",
+  projectId: "burj-al-arabic",
+  storageBucket: "burj-al-arabic.appspot.com",
+  messagingSenderId: "415069837172",
+  appId: "1:415069837172:web:326f1923dde54e0c0f7e21",
+};
+
+export default firebaseConfig;
